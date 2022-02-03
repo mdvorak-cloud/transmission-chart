a6020eced14abf6fcfa45ed1e43f2d91b01842bb
Michal Dvorak <mikee2185@gmail.com>
2022-02-04 00:22:56 +0100

    fix: yq should output json

--- init-settings.sh
+++ init-settings.sh
@@ -1,7 +1,7 @@
 function merge() {
   echo "Merging $2 into $1"
   if [ -f "$1" ]; then
-    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' "$1" "$2" >/tmp/settings.json
+    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' "$1" "$2" -o=json >/tmp/settings.json
     cp /tmp/settings.json "$1"
   else
     cp "$2" "$1"
