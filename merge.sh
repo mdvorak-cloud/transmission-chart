838f04c66d6cbf45e0fac092d4e37147503cbd3d
Michal Dvorak <mikee2185@gmail.com>
2022-02-04 15:50:43 +0100

    fix: problem was in using cp

--- merge.sh
+++ merge.sh
@@ -1,9 +1,9 @@
 function merge() {
   echo "Merging $2 into $1"
   if [ -f "$1" ]; then
-    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' "$1" "$2" -P -ojson > /tmp/settings.json
+    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' "$1" "$2" -P -ojson >/tmp/settings.json
     cp /tmp/settings.json "$1"
   else
-    cp "$2" "$1"
+    yq "$2" -P -ojson >"$1"
   fi
 }
